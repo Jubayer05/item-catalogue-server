0adf26ce996d1eb7239388111c1a7668ea451569
Jubayer05 <jubayer0504@gmail.com>
2021-04-08 08:57:33 +0600

    hide the secret info

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cors());
 
 app.use('/items', router);
 
-const CONNECTION_URL = `mongodb+srv://admin:M8HIIZGAXHGi29W9@cluster0.5wv0c.mongodb.net/itemsDB?retryWrites=true&w=majority`;
+const CONNECTION_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.PASSWORD}@cluster0.5wv0c.mongodb.net/${process.env.DB_COLLECTION}?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
 
 mongoose
